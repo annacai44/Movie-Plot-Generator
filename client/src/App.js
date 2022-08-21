5ca539eda2e44a3c640778f633b4158fc0a63f23
Anna Cai <annacai44@gmail.com>
2022-08-21 17:56:14 -0500

    trying again

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
 
   const getOpenAIResponse = () => {
     setIsLoading(true);
-    axios.get('http://localhost:8000/', {
+    axios.get('https://movieplotgenerator.herokuapp.com/', {
       params: {
         model: "text-davinci-002",
         prompt: `${isSynopsis ? prompt1 : prompt2}`,
