b884cb97d799a8fade8b4c69e0223d32a0ea747e
Anna Cai <annacai44@gmail.com>
2022-08-21 17:47:37 -0500

    trying again

--- server.js
+++ server.js
@@ -26,28 +26,24 @@ if (process.env.NODE_ENV=== "production") {
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
+
+  app.get('/', (req, res) => {
+    axios.post('https://api.openai.com/v1/completions', {
+        model: "text-davinci-002",
+        prompt: req.query.prompt,
+        max_tokens: 256,
+        temperature: parseInt(req.query.sliderValue),
+      }, {
+        headers: {
+          'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+        }
+      }).then((response) => {
+        res.json(response.data.choices[0].text)
+      }).catch((err) => {
+        console.log(err);
+      })
+  })
 }
-
-app.get('/cool', (req, res) => {
-  res.send('HI.')
-})
-
-app.get('/', (req, res) => {
-  axios.post('https://api.openai.com/v1/completions', {
-      model: "text-davinci-002",
-      prompt: req.query.prompt,
-      max_tokens: 256,
-      temperature: parseInt(req.query.sliderValue),
-    }, {
-      headers: {
-        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
-      }
-    }).then((response) => {
-      res.json(response.data.choices[0].text)
-    }).catch((err) => {
-      console.log(err);
-    })
-})
 
 // listen out to changes on our port
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
