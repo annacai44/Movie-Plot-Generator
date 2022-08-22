55e71d8742ca46cb031ba107d0e8b1774a973f5a
Anna Cai <annacai44@gmail.com>
2022-08-22 10:48:37 -0500

    update

--- server.js
+++ server.js
@@ -18,13 +18,13 @@ const app = express();
 
 app.use(cors());
 
-if (process.env.NODE_ENV=== "production") {
-  app.use(express.static("client/build"));
+// if (process.env.NODE_ENV=== "production") {
+//   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+//   });
+// }
 
 app.get('/plot', (req, res) => {
   axios.post('https://api.openai.com/v1/completions', {
