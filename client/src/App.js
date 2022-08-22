21dbb14fa74dc93ac89661db96a1f6e17aa5ef3f
Anna Cai <annacai44@gmail.com>
2022-08-21 19:01:53 -0500

    trying again

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
 
   const getOpenAIResponse = () => {
     setIsLoading(true);
-    axios.get(process.env.REACT_APP_API_URL, {
+    axios.get(`${process.env.REACT_APP_API_URL}/plot`, {
       params: {
         model: "text-davinci-002",
         prompt: `${isSynopsis ? prompt1 : prompt2}`,
