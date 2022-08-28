0d7571759eebaac10b974536a3d9d97e1fb5fae3
Anna Cai <annacai44@gmail.com>
2022-08-28 12:54:02 -0500

    added rate limiting

--- server.js
+++ server.js
@@ -17,13 +17,13 @@ app.set('trust proxy', 1);
 
 app.use(cors());
 
-if (process.env.NODE_ENV=== "production") {
-  app.use(express.static("client/build"));
+// if (process.env.NODE_ENV=== "production") {
+//   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+//   });
+// }
 
 app.get('/plot', (req, res) => {
   axios.post('https://api.openai.com/v1/completions', {
